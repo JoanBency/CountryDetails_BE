c75d4b84c683b9faf1ab8236f2605b8b24cb5068
Joan Bency <joanbency@yahoo.in>
2023-04-28 22:13:11 +0100

    Updated tests file

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -9,12 +9,65 @@ const country = {
 var len = country.name.length;
 
 
-for(var i = 0; i < len; i++) {
     describe('GET /api/country/:country', () => {
-    it('should retrieve country details', async () => {
-        const res = await request(baseURL).get(`/api/country/${country.name[i]}`);
-        expect(res.statusCode).toBe(200);
-        expect(res.body).not.toBe(undefined || null);
+        it('should retrieve country details', async () => {
+            const res = await request(baseURL).get(`/api/country/${country.name[0]}`);
+            expect(res.statusCode).toBe(200);
+            expect(res.body).not.toBe(undefined || null);
+        });
+        it('should retrieve country details', async () => {
+            const res = await request(baseURL).get(`/api/country/${country.name[1]}`);
+            expect(res.statusCode).toBe(200);
+            expect(res.body).not.toBe(undefined || null);
+        });
+        it('should retrieve country details', async () => {
+            const res = await request(baseURL).get(`/api/country/${country.name[2]}`);
+            expect(res.statusCode).toBe(200);
+            expect(res.body).not.toBe(undefined || null);
+        });
+        it('should retrieve country details', async () => {
+            const res = await request(baseURL).get(`/api/country/${country.name[3]}`);
+            expect(res.statusCode).toBe(200);
+            expect(res.body).not.toBe(undefined || null);
+        });
+        it('should retrieve country details', async () => {
+            const res = await request(baseURL).get(`/api/country/${country.name[4]}`);
+            expect(res.statusCode).toBe(200);
+            expect(res.body).not.toBe(undefined || null);
+        });
+        it('should retrieve country details', async () => {
+            const res = await request(baseURL).get(`/api/country/${country.name[5]}`);
+            expect(res.statusCode).toBe(200);
+            expect(res.body).not.toBe(undefined || null);
+        });
+        it('should retrieve country details', async () => {
+            const res = await request(baseURL).get(`/api/country/${country.name[6]}`);
+            expect(res.statusCode).toBe(200);
+            expect(res.body).not.toBe(undefined || null);
+        });
+        it('should retrieve country details', async () => {
+            const res = await request(baseURL).get(`/api/country/${country.name[7]}`);
+            expect(res.statusCode).toBe(200);
+            expect(res.body).not.toBe(undefined || null);
+        });
+        it('should retrieve country details', async () => {
+            const res = await request(baseURL).get(`/api/country/${country.name[8]}`);
+            expect(res.statusCode).toBe(200);
+            expect(res.body).not.toBe(undefined || null);
+        });
+        it('should retrieve country details', async () => {
+            const res = await request(baseURL).get(`/api/country/${country.name[9]}`);
+            expect(res.statusCode).toBe(200);
+            expect(res.body).not.toBe(undefined || null);
+        });
+        it('should retrieve country details', async () => {
+            const res = await request(baseURL).get(`/api/country/${country.name[10]}`);
+            expect(res.statusCode).toBe(200);
+            expect(res.body).not.toBe(undefined || null);
+        });
+        it('should retrieve country details', async () => {
+            const res = await request(baseURL).get(`/api/country/${country.name[11]}`);
+            expect(res.statusCode).toBe(200);
+            expect(res.body).not.toBe(undefined || null);
+        });
     });
-    });
-}
